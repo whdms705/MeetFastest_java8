9f11600ff3ce4d7e44daf3b5403f8b9b99b69024
whdms705 <whdms705@nate.com>
2018-05-27 01:51:55 +0900

    Update Sub08.java
    Add Optional examples..

--- learing/learn02/Sub08.java
+++ learing/learn02/Sub08.java
@@ -27,7 +27,41 @@ public class Sub08 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 
+	}
+	
+	//Optional로 자동차 보험회사 이름 찾기 example01
+	public String getCarInsuranceName(Optional<Person1> person){
+		return person.flatMap(Person1::getCar)
+					.flatMap(Car::getInsurance)
+					.map(Insurance::getName)
+					.orElse("Unknown");
+	}
+	
+	//두 Optional 합치는 example02
+	
+	//Person과 Car 정보를 이용하여 가장 저렴한 보험료를 제공하는 메서드있다고 가정(findCheeapestInsurance)
+	public Insurance findCheeapestInsurance(Person1 person,Car car){
+		//... 비지니스 로직이 있다고 가정	
+		return null;
+	}
+	
+	//두 Optional 합치는 방식 01
+	//전달한 값중에 하나라도 빈값이 있으면 빈 Optional 리턴
+	public Optional<Insurance> nullSafeFindCheapestInsurance01(Optional<Person1> person,Optional<Car> car){
+		if(person.isPresent() && car.isPresent()){
+			return Optional.of(findCheeapestInsurance(person.get(),car.get()));
+		}else{
+			return Optional.empty();
+		}
+	}
+	
+	//두 Optional 합치는 방식 02
+	//전달한 값중에 하나라도 빈값이 있으면 빈 Optional 리턴
+	public Optional<Insurance> nullSafeFindCheapestInsurance02(Optional<Person1> person,Optional<Car> car){
+		return person.flatMap(p -> car.map(c ->findCheeapestInsurance(p,c) ));
 	}
 
 }
