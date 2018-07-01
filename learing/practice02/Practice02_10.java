68ace8a4bb0770688f391d7718471060c1194d48
whdms705 <whdms705@nate.com>
2018-07-02 01:40:45 +0900

    Update Practice02_10.java
    Update practice content

---
+++ learing/practice02/Practice02_10.java
@@ -0,0 +1,21 @@
+package MeetFastest_java8.learing.practice02;
+
+import java.util.ArrayList;
+
+public class Practice02_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Double> list = new ArrayList<Double>();
+		list.add(1.12);
+		list.add(2.42);
+		list.add(6.12);
+		list.add(10.11);
+		
+		
+		Double result = list.stream().reduce(0.0, (a,b) -> a+b);
+		System.out.println("result : "+result/list.size());
+		//https://stackoverflow.com/questions/23658956/finding-average-using-reduce-and-collect
+	}
+
+}
