277b7011078e25a1b8d4e6c382f4282dfe8bd217
whdms705 <whdms705@nate.com>
2018-07-04 18:43:52 +0900

    Create practice02_12.java

---
+++ learing/practice02/Practice02_12.java
@@ -0,0 +1,21 @@
+package MeetFastest_java8.learing.practice02;
+
+
+/*
+ * @Content :  "2.13 병렬 스트림" 절에서 설명한 것처럼 AtomicInteger 배열을 업데이트하는 방법으로 
+ * 				병렬 Stream<String>에 있는 모든 짧은 단어의 개수를 세라.
+ * 				각 카운터를 안전하게 증가시키기 위해 원자적 메서드인 getAndIncrement를 사용한다.
+ * 
+ */
+
+public class Practice02_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 
+		 
+		 
+	}
+
+}
