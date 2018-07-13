8ae33d252fda642afe9784c616b8cfc4f1e94b0e
whdms705 <whdms705@nate.com>
2018-07-13 11:32:05 +0900

    Create Sub08.java

---
+++ learing/learn03/Sub08_2.java
@@ -0,0 +1,41 @@
+package MeetFastest_java8.learing.learn03;
+
+/*
+ * @Content : 람다식에서 예외처리 
+ * 				Sub08_2는 스레드내에서 예외가 발생하며 예외처리가 안된다.
+ */
+
+public class Sub08_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		orderAsync(()->{
+			String test1 = "qwer";
+			int result = Integer.parseInt(test1);
+			System.out.println(result);
+		},()->{
+			String test1 = "123";
+			int result = Integer.parseInt(test1);
+			System.out.println(result);
+		});
+	}
+	
+	public static void orderAsync(Runnable num1,Runnable num2) {
+		// try 구문 내에 예외발생 경우가 없다.
+		//first.run()을 하는건  스레드가 하는거지, orderAsync가 아니다.
+		//출저 : http://ghksgp92.tistory.com/125
+		try {
+			Thread t = new Thread() {
+				public void run() {
+					num1.run();
+					num2.run();
+				}
+			};
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+
+}
